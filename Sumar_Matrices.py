4fd68eb90530609f67defa873f214aa833b9195d
Xavitheforce <javier.miguelez.yague@gmail.com>
2021-12-06 18:54:17 +0100

    Update Sumar_Matrices.py

--- Sumar_Matrices.py
+++ Sumar_Matrices.py
@@ -5,16 +5,20 @@ def simpleArraySum(matriz):
     return suma
 
 def construccion_matriz(matriz):
-    filas = int(input("¿Cuántas filas y columnas tendrá la matriz?(Por favor intrduzca solo un número) "))
+    filas = int(input("¿Cuántas filas tendrá la matriz?(Por favor intrduzca solo un número) "))
     #Se podría modificar reutilizando código para que se repitiese la pregunta si no se introduce un entero
+    print("Todas las filas deben tener el mismo número de elementos.")
+    print("Si alguna fila tiene más elementos que la anterior, se eliminarán los últimos elementos de esta hasta que en cada fila haya el mismo número de elementos.")
+    print("Si alguna fila tiene menos elementos que la anterior, se añádirán 0 hasta que en cada fila haya el mismo número de elementos.")
     for i in range(filas):
         print("Introduzca los números de la fila separados solamente por un espacio")
         fila_matriz = list(map(int, input().rstrip().split()))
+        #Se podría modificar reutilizando código para que se repitiese la pregunta si no se introducen solo enteros
         matriz += [fila_matriz]
     columnas = len(matriz[0])
     for j in matriz:
-        while len(matriz[j]) > columnas:
-            matriz.pop(j)
+        while len(j) > columnas:
+            j.pop()
     print(matriz)
     return matriz
 
