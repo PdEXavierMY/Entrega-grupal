38b780127df3a7dd415277a32c8c6d2b6f8e0fc7
Xavitheforce <javier.miguelez.yague@gmail.com>
2021-12-06 18:30:52 +0100

    Update Sumar_Matrices.py

--- Sumar_Matrices.py
+++ Sumar_Matrices.py
@@ -4,7 +4,14 @@ def simpleArraySum(matriz):
         suma += i
     return suma
 
+def construccion_matriz(matriz):
+    filas = int(input("¿Cuántas filas y columnas tendrá la matriz?(Por favor intrduzca solo un número) "))
+    #Se podría modificar reutilizando código para que se repitiese la pregunta si no se introduce un entero
+    columnas = filas
+    for j in range(filas):
+        fila_matriz = list(map(int, input().rstrip().split()))
+    return matriz
+
 if __name__ == '__main__':
-    matriz = list(map(int, input().rstrip().split()))
-    print(matriz)
+    matriz = []
     result = simpleArraySum(matriz)
