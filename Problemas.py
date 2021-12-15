ee40d240998153dd04abe014866bba2de429c6bb
Pelayo <phuermij@myuax.com>
2021-12-15 22:23:36 +0100

    Update Problemas.py

---
+++ Problemas.py
@@ -0,0 +1,16 @@
+import random
+
+a = []
+for i in range(3):
+    a.append(random.randint(0,100))
+
+b = []
+for i in range(3):
+    b.append(random.randint(0,100))
+
+def compareTriplets():
+    score1 = 0
+    score2 = 0
+    
+
+
