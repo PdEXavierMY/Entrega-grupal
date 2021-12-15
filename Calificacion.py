c74a5ab1b8b0b2621caab2fc5b372cb33f28c3df
Pelayo <phuermij@myuax.com>
2021-12-15 19:55:37 +0100

    Update Calificaciones.py

--- Calificacion.py
+++ Calificacion.py
@@ -8,5 +8,11 @@ def gradingStudents():
             n = n + 5
         
         diferencia = n - nota
+
+        if diferencia > 3:
+            print("Su nota es un aprovado, pero no fue redondeada: " + str(nota))
         
+        else:
+            print("Su nota es un aprovado y fue redondeada: " + str(nota))
             
+
