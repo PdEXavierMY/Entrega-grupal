96d3c072037c7bc2637db5e323d61e2a606ca6b2
Pelayo <phuermij@myuax.com>
2021-12-15 20:11:34 +0100

    Update Calificacion.py

--- Calificacion.py
+++ Calificacion.py
@@ -1,11 +1,11 @@
 def gradingStudents(nota):
-#Primero comparamos si la nota es un suspenso o un aprovado
+#Primero comparamos si la nota es un suspenso o un aprobado
     if nota < 40:
         #Si es suspenso simplemente devuelve la nota tal y como se introdujo
         print("Su nota es un suspenso: " + str(nota))
     
     else:
-        #Si es aprovado puede ser redondeada o no
+        #Si es aprobado puede ser redondeada o no
         n = 0
 
         #Este bucle  calculara el múltiplo de 5 siguiente a la nota
@@ -18,13 +18,13 @@ def gradingStudents(nota):
         #Si la diferencia es mayor que 3 la nota se queda igual
         if diferencia > 3:
             nota = nota
-            print("Su nota es un aprovado, pero no fue redondeada: " + str(nota))
+            print("Su nota es un aprobado, pero no fue redondeada: " + str(nota))
         
         #Si es menor que 3 la nota se redondea sumando la diferencia previamente calculada
         else:
             nota = nota + diferencia
-            print("Su nota es un aprovado y fue redondeada a un: " + str(nota))
-            
+            print("Su nota es un aprobado y fue redondeada a un: " + str(nota))
+
 
 if __name__ == "__main__":
     nota = int(input("Introduce tu nota: "))
