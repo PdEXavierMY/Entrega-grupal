34f94f8b125859feae389f30b4883ee51fd2da86
Pelayo <phuermij@myuax.com>
2021-12-15 18:44:13 +0100

    Update Pirámide.py

--- Pirámide.py
+++ Pirámide.py
@@ -1,1 +1,5 @@
-n = int(input("Introduce la altura de la pirámide: "))
+#Esto pedirá al usuario un número para definir la altura y base que tendrá nuestra pirámide
+n = int(input("Introduce la altura de la pirámide: ")) 
+
+def piramide():
+    
