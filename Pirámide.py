6fdf9c62e6a3c1ae96627ed7aa0c514f88260cad
Pelayo <phuermij@myuax.com>
2021-12-15 19:01:42 +0100

    Update Pirámide.py

--- Pirámide.py
+++ Pirámide.py
@@ -1,10 +1,11 @@
-#Esto pedirá al usuario un número para definir la altura y base que tendrá nuestra pirámide
-n = int(input("Introduce la altura de la pirámide: ")) 
-
 def piramide():
     #Esto recorre las n filas de la pirámide poniendo los espacios y hastags en el lugar correspondiente para formar la pirámide
     for i in range(n):
-        print(" " * (n - i - 1) + "#" * (i + 1))
+        print(" " * (n - i - 1) + "# " * (i + 1))
+        print("\n")
         
-
-piramide()
+if __name__ == "__main__":
+    #Esto pedirá al usuario un número para definir la altura y base que tendrá nuestra pirámide
+    n = int(input("Introduce la altura de la pirámide: ")) 
+    print("\n")
+    piramide()
