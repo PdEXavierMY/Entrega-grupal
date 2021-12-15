68714f5dfb63086fff88c6fce32e2aad4ca8e82e
Xavitheforce <javier.miguelez.yague@gmail.com>
2021-12-15 19:23:40 +0100

    Update Juego_Piedras.py

--- Juego_Piedras.py
+++ Juego_Piedras.py
@@ -2,43 +2,54 @@
 
 import random
 
-def casospiedrasfavorables(npiedras):
+def casospiedras(npiedras):
     if npiedras == 2:
         npiedras -= 2
+        print("2 piedras")
     elif npiedras == 3:
         npiedras -= 3
+        print("3 piedras")
     elif npiedras == 4:
         npiedras -= 3
+        print("3 piedras")
     elif npiedras == 5:
         npiedras -= 5
+        print("5 piedras")
     elif npiedras == 6:
         npiedras -= 5
+        print("5 piedras")
     elif npiedras == 9:
         npiedras -= 2
+        print("2 piedras")
     elif npiedras == 10:
-        npiedras -= random.choice(2, 3)
+        n = random.choice(2, 3)
+        npiedras -= n
+        print(str(n) + " piedras")
     elif npiedras == 11:
         npiedras -= 3
+        print("3 piedras")
     elif npiedras == 12:
         npiedras -= 5
+        print("5 piedras")
     elif npiedras == 13:
         npiedras -= 5
+        print("5 piedras")
 
 def juegopiedras(npiedras):
     #movimientosposibles = [2, 3, 5]
     turno = 1
-    while turno == 1: #Jugador 1
-        finmovimiento = False
-        if npiedras < 2:
-            print("Jugador 1 ha perdido, no puede extraer piedras")
-            break
-        else:
-            if npiedras < 13:
-                casospiedrasfavorables(npiedras)
-                print("Jugador 1 quita ", end="")
-                print("Quedan " + str(npiedras) + " piedras.")
+    while True:
+        if turno == 1: #Jugador 1
+            if npiedras < 2:
+                print("Jugador 1 ha perdido, no puede extraer piedras")
+                break
             else:
-                if finmovimiento == True:
+                if npiedras < 20:
+                    casospiedras(npiedras)
+                    print("Jugador 1 quita ", end="")
+                    print("Quedan " + str(npiedras) + " piedras.")
+                else:
+                    movimiento = random.choice(2, 3, 5)
+                    npiedras -= movimiento
+                    print("Jugador 1 quita " + str(movimiento) + " piedras.")
                     turno = 2
-    while turno == 2: #Jugador 2
-    
