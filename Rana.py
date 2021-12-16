3ec1418e8b9d6ef793acf49c0b48e546e52eaacd
Xavitheforce <javier.miguelez.yague@gmail.com>
2021-12-16 19:42:01 +0100

    creacion laberinto

--- Rana.py
+++ Rana.py
@@ -3,9 +3,9 @@
 
 muro = ((0,5), (1,3), (1,4), (2,2), (2,3), (2,7), (3,3), (4,3))
 bombas = ((3,2),(3,6), (3,7))
-tunel1 = ((0,2), (7,0))
+tunel1 = ((0,2), (4,0))
 tunel2 = ((0,4), (2,4))
-salidas = ((0,7), (7,7))
+salidas = ((0,7), (4,7))
 
 estructura = len(muro)
 filas = 5
@@ -15,20 +15,40 @@ laberinto = [[]]
 for l in range(filas - 1):
     laberinto += [[]]
 
-def comparacion(i, j):
-    haymuro = False
-    for valor in range(estructura):
-        if muro[valor] == (i, j):
+def obstaculos(i, j):
+    hayobstaculo = False
+    for valor in muro:
+        if valor == (i, j):
             laberinto[i].append("X")
-            haymuro = True
+            hayobstaculo = True
             break
-    if haymuro == False:
+    for valor in bombas:
+        if valor == (i, j):
+            laberinto[i].append("B")
+            hayobstaculo = True
+            break
+    for valor in tunel1:
+        if valor == (i, j):
+            laberinto[i].append("t")
+            hayobstaculo = True
+            break
+    for valor in tunel2:
+        if valor == (i, j):
+            laberinto[i].append("T")
+            hayobstaculo = True
+            break
+    for valor in salidas:
+        if valor == (i, j):
+            laberinto[i].append("S")
+            hayobstaculo = True
+            break
+    if hayobstaculo == False:
         laberinto[i].append(" ")
     return laberinto
 
 for i in range(filas):
     for j in range(columnas):
-        comparacion(i, j)
+        obstaculos(i, j)
 
 def despliegue_laberinto():
     for z in range(len(laberinto) - 1):
