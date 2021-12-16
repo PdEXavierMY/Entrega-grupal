dbd8f386002df749dc82c2c2ac2123be6d1d61a2
Xavitheforce <javier.miguelez.yague@gmail.com>
2021-12-16 17:44:45 +0100

    Planteamiento

---
+++ Manzanas_y_Naranjas.py
@@ -0,0 +1,12 @@
+#Definir la casa y árboles
+#Recta(Eje x) de 0 a 50
+#Casa en el intervalo x = (25,30)
+#Manzano en x = 20
+#Naranjo en x = 35
+
+import random
+
+from Suma_Número_muygrande import construccion_matriz
+
+contadormanzanas = 0
+contadornaranjas = 0
