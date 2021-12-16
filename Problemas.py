85e59c485ec26fd53b33105ff6a0f8ce62b6de1c
Pelayo <phuermij@myuax.com>
2021-12-16 10:20:02 +0100

    Update Problemas.py

--- Problemas.py
+++ Problemas.py
@@ -3,10 +3,12 @@ import random
 a = []
 for i in range(3):
     a.append(random.randint(0,100))
+print("La calificación del desafío de Lucía es: " + str(a))
 
 b = []
 for i in range(3):
     b.append(random.randint(0,100))
+print("La calificación del desafío de Carlos es: " + str(b))
 
 def compareTriplets():
     score1 = 0
@@ -19,14 +21,14 @@ def compareTriplets():
         elif a[i] < b[i]:
             score2 = score2 + 1
 
-    lista = [score1, score2]
+    lista = ["Lucía: " + str(score1), "Carlos: " + str(score2)]
     print("Las puntuaciones son: " + str(lista))
     
 
 
 
 
-if __name__ == "__Main__":
+if __name__ == "__main__":
     compareTriplets()
 
 
