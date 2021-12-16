de6f19e27ad400c300ad66752267f8408f4ef04b
Xavitheforce <javier.miguelez.yague@gmail.com>
2021-12-16 19:11:55 +0100

    Update Manzanas_y_Naranjas.py

--- Manzanas_y_Naranjas.py
+++ Manzanas_y_Naranjas.py
@@ -1,14 +1,13 @@
-#Definir la casa y árboles
-#Recta(Eje x) de 0 a 50
+#Definir la casa y árboles:
 #Casa en el intervalo x = (25,30)
 #Manzano en x = 20
 #Naranjo en x = 35
 
 import random
 
-CASA = (25, 30)
-MANZANO = 20
-NARANJO = 35
+CASA = tuple(map(int, input("Introduce el intervalo de x en el que está la casa de Sam: ").rstrip().split()))
+MANZANO = int(input("Introduce la posición del manzano: "))
+NARANJO = int(input("Introduce la posición del naranjo: "))
 contadormanzanas = 0
 contadornaranjas = 0
 manzanascaidas = int(input("Introduce el número de manzanas que se han caido del árbol: "))
@@ -16,14 +15,14 @@ naranjascaidas = int(input("Introduce el número de naranjas que se han caido de
 
 def contarfrutas(contadormanzanas, contadornaranjas):
     for i in range(manzanascaidas):
-        valor_caida_m = random.randint(-15,15)
+        valor_caida_m = random.randint(-15,15) #Se puede ampliar aunque reduce la posibilidad de caer en la casa
         manzana = MANZANO - valor_caida_m
         print("La " + str(i +1) + " manzana ha caido en el punto x = " + str(manzana))
         if CASA[0] <= manzana <= CASA[1]:
             contadormanzanas += 1
 
     for j in range(naranjascaidas):
-        valor_caida_n = random.randint(-15,15)
+        valor_caida_n = random.randint(-15,15) #Se puede ampliar aunque reduce la posibilidad de caer en la casa
         naranja = MANZANO - valor_caida_n
         print("La " + str(j +1) + " naranja ha caido en el punto x = " + str(naranja))
         if CASA[0] <= naranja <= CASA[1]:
