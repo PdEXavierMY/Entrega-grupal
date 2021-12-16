402557792471f20cfd0438b9eb2ae97681e8cd54
Xavitheforce <javier.miguelez.yague@gmail.com>
2021-12-16 18:02:49 +0100

    Update Manzanas_y_Naranjas.py

--- Manzanas_y_Naranjas.py
+++ Manzanas_y_Naranjas.py
@@ -14,15 +14,21 @@ contadornaranjas = 0
 manzanascaidas = int(input("Introduce el número de manzanas que se han caido del árbol: "))
 naranjascaidas = int(input("Introduce el número de naranjas que se han caido del árbol: "))
 
-def contarfrutas():
+def contarfrutas(contadormanzanas, contadornaranjas):
     for i in range(manzanascaidas):
         valor_caida_m = random.randint(-15,15)
         manzana = MANZANO - valor_caida_m
         if CASA[0] <= manzana <= CASA[1]:
             contadormanzanas += 1
+    print("En total han caido " + str(contadormanzanas) + " manzanas en la casa de Sam.")
 
     for j in range(naranjascaidas):
         valor_caida_n = random.randint(-15,15)
         naranja = MANZANO - valor_caida_m
         if CASA[0] <= naranja <= CASA[1]:
             contadornaranjas += 1
+
+    print("En total han caido " + str(contadornaranjas) + " naranjas en la casa de Sam.")
+
+if __name__ == '__main__':
+    contarfrutas()
